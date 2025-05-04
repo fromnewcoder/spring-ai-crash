7a1b53f70f10e7f03631adc4d54527d366c1b20e
jq <jq@gmail.com>
2025-05-04 18:22:43 +0800

    initprojtect

---
+++ src/test/java/com/ai/springai/crash/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.springai.crash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
